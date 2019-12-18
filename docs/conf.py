c7d473e4bf235b46603de755577fd21e0cd77730
codertimo <codertimo@gmail.com>
2019-12-19 05:51:28 +0900

    change sphinx theme
    Signed-off-by: codertimo <codertimo@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "pandas_sphinx_theme"
 
 master_doc = "index"
 
@@ -57,3 +57,34 @@ master_doc = "index"
 html_static_path = ["_static"]
 
 autodoc_mock_imports = []
+
+# import sphinx_material
+
+# # Required theme setup
+# extensions.append("sphinx_material")
+# html_theme = "sphinx_material"
+# html_theme_path = sphinx_material.html_theme_path()
+# html_context = sphinx_material.get_html_context()
+
+# # Material theme options (see theme.conf for more information)
+# html_theme_options = {
+#     # Set the name of the project to appear in the navigation.
+#     "nav_title": "Project Name",
+#     # Set you GA account ID to enable tracking
+#     "google_analytics_account": "UA-XXXXX",
+#     # Specify a base_url used to generate sitemap.xml. If not
+#     # specified, then no sitemap will be built.
+#     "base_url": "https://project.github.io/project",
+#     # Set the color and the accent color
+#     "color_primary": "blue",
+#     "color_accent": "light-blue",
+#     # Set the repo location to get a badge with stats
+#     "repo_url": "https://github.com/project/project/",
+#     "repo_name": "Project",
+#     # Visible levels of the global TOC; -1 means unlimited
+#     "globaltoc_depth": 3,
+#     # If False, expand all TOC entries
+#     "globaltoc_collapse": False,
+#     # If True, show hidden TOC entries
+#     "globaltoc_includehidden": False,
+# }

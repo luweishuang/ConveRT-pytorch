08d65579e0eb67169b59b208b9e2d00597d43744
codertimo <codertimo@gmail.com>
2019-12-18 23:50:14 +0900

    merge self attention to model.py
    Signed-off-by: codertimo <codertimo@gmail.com>

--- convert/model.py
+++ convert/model.py
@@ -1,13 +1,13 @@
+import math
+from typing import NamedTuple, Optional
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as fnn
-
 from torch.nn.modules.normalization import LayerNorm
 from torch.nn.modules.transformer import MultiheadAttention
 
-from typing import NamedTuple, Optional
 from .config import ConveRTModelConfig
-from .attention import SelfAttention
 
 
 class EncoderInput(NamedTuple):
@@ -15,6 +15,40 @@ class EncoderInput(NamedTuple):
     attention_mask: torch.FloatTensor
     position_ids: torch.LongTensor = None
     input_lengths: torch.LongTensor = None
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, config: ConveRTModelConfig):
+        super().__init__()
+
+        self.config = config
+        self.query = nn.Linear(config.num_embed_hidden, config.num_attention_project)
+        self.key = nn.Linear(config.num_embed_hidden, config.num_attention_project)
+        self.value = nn.Linear(config.num_embed_hidden, config.num_attention_project)
+
+        self.softmax = nn.Softmax(dim=-1)
+        self.output_projection = nn.Linear(config.num_attention_project, config.num_embed_hidden)
+
+    def forward(self, query, attention_mask=None, return_attention=False):
+        _query = self.query.forward(query)
+        _key = self.key.forward(query)
+        _value = self.value.forward(query)
+
+        # scaled dot product (https://www.aclweb.org/anthology/N18-2074.pdf Fig.2)
+        attention_scores = torch.matmul(_query, _key.transpose(1, 2))
+        attention_scores = attention_scores / math.sqrt(self.config.num_attention_project)
+
+        if attention_mask is not None:
+            extended_attention_mask = attention_mask.to(dtype=next(self.parameters()).dtype)  # fp16 compatibility
+            extended_attention_mask = (1.0 - extended_attention_mask.unsqueeze(-1)) * -10000.0
+            attention_scores = attention_scores + extended_attention_mask
+
+        attention_weights = self.softmax(attention_scores)
+        weighted_value = torch.matmul(attention_weights, _value)
+
+        output_value = self.output_projection.forward(weighted_value)
+
+        return (output_value, attention_weights) if return_attention else output_value
 
 
 class ConveRTInnerFeedForward(nn.Module):
--- tests/test_model.py
+++ tests/test_model.py
@@ -8,10 +8,24 @@ from convert.model import (
     ConveRTEncoderLayer,
     ConveRTSharedEncoder,
     EncoderInput,
+    SelfAttention,
 )
 
 config = ConveRTModelConfig()
 BATCH_SIZE, SEQ_LEN = 3, 5
+
+
+def test_self_attention():
+    BATCH_SIZE, SEQ_LEN = 4, 5
+    config = ConveRTModelConfig()
+    attention = SelfAttention(config)
+
+    query = torch.rand(BATCH_SIZE, SEQ_LEN, config.num_embed_hidden)
+    attn_mask = torch.ones(query.size()[:-1], dtype=torch.float)
+    output, attn = attention.forward(query, attn_mask, return_attention=True)
+
+    assert output.size() == (BATCH_SIZE, SEQ_LEN, config.num_embed_hidden)
+    assert attn.size() == (BATCH_SIZE, SEQ_LEN, SEQ_LEN)
 
 
 def test_convert_embedding():

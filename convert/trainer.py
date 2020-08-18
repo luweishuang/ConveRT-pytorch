e209d700e9b3fcd042b8f957792469adff117deb
luweishuang <pifuchan@gmail.com>
2020-08-18 13:54:04 +0800

    gpu training

--- convert/trainer.py
+++ convert/trainer.py
@@ -37,9 +37,12 @@ class ConveRTTrainer:
         self.train_dataloader: DataLoader = train_dataloader
         self.test_dataloader: DataLoader = test_dataloader
         self.device_count: int = torch.cuda.device_count() if self.train_config.use_data_paraller else 1
+        if not torch.cuda.is_available():                     # cpu
+            self.device_count = 1
 
     def train(self):
         for epoch_id in range(self.train_config.epochs):
+            print("epoch_id = ", epoch_id)
             self.train_epoch(epoch_id)
             self.evaluation(epoch_id)
             self.save_model(epoch_id)
@@ -47,14 +50,17 @@ class ConveRTTrainer:
     def train_epoch(self, epoch_id: int):
         total_steps = len(self.train_dataloader)
         for step_id, feature in enumerate(self.train_dataloader):
+            print("step_id = ", step_id)
             start_time = time.time()
 
             self.optimizer.zero_grad()
             context_embed, reply_embed = self.model.forward(feature.context, feature.reply)
             query_reply_similarity = self.criterion.forward(context_embed, reply_embed)
+            print("criterion.forward")
             loss, correct_count, total_count = calculate_query_reply_matching_loss(
                 query_reply_similarity, self.train_config.split_size, self.device_count
             )
+            print("calculate_query_reply_matching_loss")
             accuracy = float(correct_count) / total_count
 
             loss.backward()
